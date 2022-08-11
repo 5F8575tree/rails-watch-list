79cb2763637c5609794eecc1ac5daca0eac29301
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-11 12:55:32 +0900

    added bookmarks controller, added lists controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # A user can create a new list
+  resources :lists, only: %i[index show new create]
+  resources :bookmarks
 end
