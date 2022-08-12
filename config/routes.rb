ec48c819112cfa08519ecbb6149b84ec0dc71978
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-12 11:15:21 +0900

    altered root to path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # A user can create a new list
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
